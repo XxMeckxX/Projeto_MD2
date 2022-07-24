62d908ffc918991cbd48dbb21843e1ca0f240cc8
Érick Vieira Martins Alves <kvalves99@gmail.com>
2022-07-24 10:17:18 -0300

    Mudanças
    Foi feito algumas mudanças.
    primeiro: foram adicionados módulos para melhorar a visualização da tabela no vscode.
    segundo: Foi utilizado o módulo pandas para mexer com csv,facilitando  e diminuindo as linhas de código.
    Terceiro: Foi trocado o modo que as informações eram guardadas,deixaram de serem armazenadas em listas de listas e passaram a ser armazenadas em um dincionário na qual cada chave continha uma lista.

--- PesquisaErick.py
+++ PesquisaErick.py
@@ -1,16 +1,32 @@
-from asyncore import write
-import csv
+import tabulate
 import datetime
+import pandas as pd
 
 
-class Perguntas():
-    resposta1 = 1
-    resposta2 = 1
-    resposta3 = 1
-    resposta4 = 1
+class Questionario():
+    __Idade = []
+    __Genero = []
+    __Resposta1 = []
+    __Resposta2 = []
+    __Resposta3 = []
+    __Resposta4 = []
+    __Data = []
+    __Hora = []
+
+    __dic = {
+        'Idade': __Idade,
+        'Gênero':__Genero,
+        'Pergunta1':__Resposta1,
+        'Pergunta2':__Resposta2,
+        'Pergunta3':__Resposta3,
+        'Pergunta4':__Resposta4,
+        'Data da Resposta':__Data,
+        'Horário da Resposta':__Hora
+    }
     def __init__(self,idade,genero):
-        self.idade = idade
-        self.genero = genero
+        self.__Idade.append(str(idade))
+        self.__Genero.append(str(genero))
+        
 
     def respostas(self):
         while True:
@@ -19,8 +35,15 @@ class Perguntas():
                 if (escolha != 1) and (escolha!=2) and (escolha != 3):
                     print('Escolha inválida.Tente novamente...')
                 else:
-                    self.resposta1 = escolha
-                    break
+                    if escolha == 1:
+                        self.__Resposta1.append('Sim')
+                        break
+                    elif escolha == 2:
+                        self.__Resposta1.append('Não')
+                        break
+                    elif escolha == 3:
+                        self.__Resposta1.append('Não sei responder')
+                        break
             except:
                 print('Escolha inválida.Tente  novamente...')
         while True:
@@ -29,8 +52,15 @@ class Perguntas():
                 if (escolha != 1) and (escolha!=2) and (escolha != 3):
                     print('Escolha inválida.Tente novamente...')
                 else:
-                    self.resposta2 = escolha
-                    break
+                    if escolha == 1:
+                        self.__Resposta2.append('Sim')
+                        break
+                    elif escolha == 2:
+                        self.__Resposta2.append('Não')
+                        break
+                    elif escolha == 3:
+                        self.__Resposta2.append('Não sei responder')
+                        break
             except:
                 print('Escolha inválida.Tente  novamente...')
         while True:
@@ -39,8 +69,15 @@ class Perguntas():
                 if (escolha != 1) and (escolha!=2) and (escolha != 3):
                     print('Escolha inválida.Tente novamente...')
                 else:
-                    self.resposta3 = escolha
-                    break
+                    if escolha == 1:
+                        self.__Resposta3.append('Sim')
+                        break
+                    elif escolha == 2:
+                        self.__Resposta3.append('Não')
+                        break
+                    elif escolha == 3:
+                        self.__Resposta3.append('Não sei responder')
+                        break
             except:
                 print('Escolha inválida.Tente  novamente...')
         while True:
@@ -49,46 +86,78 @@ class Perguntas():
                 if (escolha != 1) and (escolha!=2) and (escolha != 3):
                     print('Escolha inválida.Tente novamente...')
                 else:
-                    self.resposta4 = escolha
-                    break
+                    if escolha == 1:
+                        self.__Resposta4.append('Sim')
+                        break
+                    elif escolha == 2:
+                        self.__Resposta4.append('Sim')
+                        break
+                    elif escolha == 3:
+                        self.__Resposta4.append('Não sei responder')
+                        break
             except:
                 print('Escolha inválida.Tente  novamente...')
         exato = datetime.datetime.now()
         data = exato.strftime('%d/%m/%y')
-        momento = exato.strftime("%I:%M%p")
-        return [self.idade,self.genero,self.resposta1,self.resposta2,self.resposta3,self.resposta4,data,momento]
+        self.__Data.append(data)
+        momento = exato.strftime("%I:%M %p")
+        self.__Hora.append(momento)
 
-# def inserirRespostas(pessoa = None):
-#     with open('teste.csv', mode = 'a') as arquivo:
-#         for coluna in leitor:
-#             if linhas == 0:
-#                 conc = ' '.join(coluna)
-#                 print(f'\tColunas: {conc}')
-#                 linhas += 1
-#             else:
-                
-#                 print(f'\tPessoa {linhas}: {coluna}')
-#                 linhas += 1
-#         print(f'Foram lidas {linhas} linhas.') 
+       
+    def get_info(self):
+        return self.__dic
 
-pessoa = []
-print('Programa iniciando')
+        
+
+
+class Tabela():
+    def __init__(self,dicionario):
+        self.__dicionario = dicionario
+
+    def criarTabela(self): # Equivalente a um set_tabela
+        self.__pesquisa = pd.DataFrame(self.__dicionario,index= [f'Participante {i+1}:' for i in range(len(self.__dicionario['Idade']))])
+        self.__pesquisa.to_csv('teste.csv')    
+    def lerTabela(self): # Equivalente a um get_tabela
+        print(tabulate.tabulate(self.__pesquisa, headers = 'keys',tablefmt = 'github'))
+        print(self.__pesquisa)
+
+count = 0
+print('Iniciando a pesquisa...')
 while True:
+    count += 1
     while True:
-        try:
-            idade = int(input('Qual a sua idade?(Digite 0 para sair do programa.)\n'))
-            if idade >= 0 and idade <= 120:
-                break
+        idade = input(f'Participante {count},qual a sua idade?(Digite 00 para sair do programa.)\n')
+        if idade != '00': # Verifica se é 00
+            if idade.isnumeric(): # Verifica se é um número escrito em formato de string
+                idade = int(idade) # Se for,transforma em inteiro e faz o resto.
+                if idade >= 0 and idade <= 120:
+                    break
+                else:
+                    print('Idade inválida,digite um número inteiro positivo entre 0 e 120')
             else:
-                print('Idade inválida,digite um número inteiro positivo entre 0 e 120')
-        except ValueError:
-            print('Idade inválida,digite um número Inteiro positivo por favor.')
-    if idade == 0:
+                print('Idade inválida,digite um número Inteiro positivo por favor.')
+        else:
+            break
+    if idade == '00':
         print('Saindo do questionário...')
         break
-    genero = input('Qual o seu gênero? ')
-    convidado = Perguntas(idade,genero)
-    pessoa.append(convidado.respostas())
-pessoa    
-    
+    while True:
+        try:
+            escolha_genero = int(input('Qual o seu gênero?\n[1]Masculino\n[2]Feminino\n[3]LGBTQIA+\3\n'))
+            if escolha_genero != 1 and escolha_genero != 2 and escolha_genero != 3:
+                print('Escolha inválida,tente novamente...')
+            else:
+                break
+        except ValueError:
+            print('Digite um número entre 1 e 3 para fazer sua escolha.')
+    convidado = Questionario(idade,escolha_genero)
+    convidado.respostas()
 
+
+
+
+ 
+
+tabela = Tabela(convidado.get_info()) # Entro no objeto tabela passando como argumento um dicionário
+tabela.criarTabela()   # Crio a tabela com o pandas
+tabela.lerTabela() # Leio oque foi criado
